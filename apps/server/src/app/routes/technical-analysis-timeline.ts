56a417ec540f18e676e08fa9536dd70c9369b8ef
Gaurav Phansekar <coder19855@gmail.com>
2026-06-14 12:19:45 +0200

    feat(*): add benchmark tool and other improvements

--- apps/server/src/app/routes/technical-analysis-timeline.ts
+++ apps/server/src/app/routes/technical-analysis-timeline.ts
@@ -28,7 +28,6 @@ import { ResponseStatus } from '../types';
 import {
   TechnicalAnalysisTimelineResponse,
   TimelinePoint,
-  TradeAction,
 } from '../types/technical-analysis';
 import { getStyleScoringConfig, TradingStyle } from '../trading-style';
 
